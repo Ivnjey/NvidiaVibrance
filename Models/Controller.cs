19bf348ee363d238afd1a05349a98077775635e1
ightson <ightson@gmail.com>
2025-05-29 19:59:27 +0300

    init

---
+++ Models/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NvAPIWrapper;
+using NvAPIWrapper.Display;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var controller = new NvidiaVibrance.NvidiaDisplayController();
+    }
+}
+namespace NvidiaVibrance
+{
+    public class NvidiaDisplayController
+    {
+        private Display displayDevice;
+        // private uint displayId;
+
+        public NvidiaDisplayController()
+        {
+            NVIDIA.Initialize();
+            Display[] displays = Display.GetDisplays();
+            if (displays.Length == 0)
+            {
+                throw new Exception("No NVIDIA display devices found.");
+            }
+            displayDevice = displays[0];
+        }
+
+        public void SetDigitalVibrance(int level)
+        {
+            if (level < 0 || level > 100)
+            {
+                throw new ArgumentOutOfRangeException("Digital vibrance level must be between 0 and 100.");
+            }
+            var colorControl = displayDevice.DigitalVibranceControl;
+            colorControl.CurrentLevel = level;
+        }
+
+        public int GetDigitalVibrance()
+        {
+            return displayDevice.DigitalVibranceControl.CurrentLevel;
+        }
+        public void SetDigitalVibranceDefault()
+        {
+            int colorControl = displayDevice.DigitalVibranceControl.DefaultLevel;
+            SetDigitalVibrance(colorControl);
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NvidiaVibrance.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
