20803d0f7177ec495d983f42c8da83bdec62f06f
Ivnjey <you@example.com>
2025-05-30 21:22:54 +0300

    add mouse wheel

--- Views/VibranceControlWindow.axaml.cs
+++ Views/VibranceControlWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using NvidiaVibrance.ViewModels;
 
 namespace NvidiaVibrance.Views;
 
@@ -8,4 +10,19 @@ public partial class VibranceControlWindow : Window
     {
         InitializeComponent();
     }
-} 
+
+    private void OnSliderMouseWheel(object? sender, PointerWheelEventArgs e)
+    {
+        if (DataContext is MainWindowViewModel viewModel && sender is Slider slider)
+        {
+            int change = e.Delta.Y > 0 ? 1 : -1;
+            int newValue = viewModel.CurrentVibrance + change;
+
+            if (newValue < slider.Minimum) newValue = (int)slider.Minimum;
+            if (newValue > slider.Maximum) newValue = (int)slider.Maximum;
+
+            viewModel.CurrentVibrance = newValue;
+            e.Handled = true;
+        }
+    }
+}
